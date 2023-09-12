c887855b6cee2e2327bb8e31c71ef9422d4435f4
Charles-04 <charlesmir04@gmail.com>
2023-09-12 10:34:56 +0100

    Chore : Abstracted Persistence Logic from domain logic

--- TaskManagement.Domain/Entities/UserProfile.cs
+++ TaskManagement.Domain/Entities/UserProfile.cs
@@ -10,9 +10,11 @@ namespace TaskManager.Domain.Entities
     public class UserProfile
     {
         public string Id { get; set; }
+        public string UserId { get; set; }
         public string FullName { get; set; }
         public Gender Gender { get; set; }
         public bool Active { get; set; }
+        public AppUser User { get; set; }
         public ICollection<Task> Tasks { get; set; }
         public ICollection<Project> Projects { get; set; }
         public DateTime Birthday { get; set; }
---
+++ TaskManagement.Domain/Shared/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Domain.Shared
+{
+    public record Response<T>
+    {
+        public string Message { get; init; }
+        public T Result { get; init; }
+        public bool Success { get; init; }
+    }
+}
