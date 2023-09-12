a10f7b8ea7cbfc535aec19aaf381f23f1faafab4
Charles-04 <charlesmir04@gmail.com>
2023-09-12 10:35:30 +0100

    Feat : Added Jwt Authenticator

---
+++ TaskManager.Persistence/Context/TaskAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace TaskManager.Persistence.Context
+{
+    public class TaskAppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public TaskAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
